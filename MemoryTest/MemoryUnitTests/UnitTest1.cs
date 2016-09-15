5ce96e309aa902b907ef5d10f07d83a14a493898
Kiwiov <hugo.hansen@lund.ntig.se>
2016-09-15 08:43:20 +0200

    Added Test Class.

---
+++ MemoryTest/MemoryUnitTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MemoryLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MemoryUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arange
+           
+
+            //Act
+            
+
+            //Assert
+            
+        }
+    }
+}

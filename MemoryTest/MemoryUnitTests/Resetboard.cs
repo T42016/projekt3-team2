f14dd9aeec5fad29fa5283da014ff5f9a6fe3b61
Marrezet <martin.dahren@lund.ntig.se>
2016-09-15 10:12:26 +0200

    ResetBoardShouldInitializePositionInfoWithX, ResetBoardShouldInitializePositionInfoWithY

--- MemoryTest/MemoryUnitTests/Resetboard.cs
+++ MemoryTest/MemoryUnitTests/Resetboard.cs
@@ -1,14 +1,68 @@
 ﻿using System;
+using MemoryLogic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MemoryUnitTests
 {
     [TestClass]
-    public class UnitTest2
+    public class ResetBoard
     {
+        MemoryGame _Resettest = new MemoryGame(4, 4);
+
         [TestMethod]
-        public void TestMethod1()
+        public void ResetBoardShouldCreatePositionInfoForAllBoardPositions()
         {
+            //Arrange via setup
+
+            //Act
+            _Resettest.ResetBoard();
+
+            //Assert
+            Assert.IsNotNull(_Resettest.GetCoordinate(0, 0));
+
+            for (int y = 0; y < _Resettest.SizeY; y++)
+            {
+                for (int x = 0; x < _Resettest.SizeX; x++)
+                {
+                    Assert.IsInstanceOfType(_Resettest.GetCoordinate(x, y), typeof(PositionInfo));
+                }
+            }
+        }
+        [TestMethod]
+        public void ResetBoardShouldInitializePositionInfoWithX()
+        {
+            //Arrange via setup
+
+            //Act
+            _Resettest.ResetBoard();
+
+            //Assert
+            for (int y = 0; y < _Resettest.SizeY; y++)
+            {
+                for (int x = 0; x < _Resettest.SizeX; x++)
+                {
+                    Assert.AreEqual(x, _Resettest.GetCoordinate(x, y).X);
+                }
+            }
+            Assert.IsNotNull(_Resettest.GetCoordinate(0, 0));
+        }
+        [TestMethod]
+        public void ResetBoardShouldInitializePositionInfoWithY()
+        {
+            //Arrange via setup
+
+            //Act
+            _Resettest.ResetBoard();
+
+            //Assert
+            for (int y = 0; y < _Resettest.SizeY; y++)
+            {
+                for (int x = 0; x < _Resettest.SizeX; x++)
+                {
+                    Assert.AreEqual(y, _Resettest.GetCoordinate(x, y).Y);
+                }
+            }
+            Assert.IsNotNull(_Resettest.GetCoordinate(0, 0));
         }
     }
 }
--- MemoryTest/MemoryUnitTests/UnitTest1.cs
+++ MemoryTest/MemoryUnitTests/UnitTest1.cs
@@ -12,31 +12,4 @@ namespace MemoryUnitTests
 
 
     }
-    [TestClass]
-    public class ResetBoard
-    {
-        MemoryGame _Resettest = new MemoryGame(4, 4);
-
-        [TestMethod]
-        public void ResetBoardShouldCreatePositionInfoForAllBoardPositions()
-        {
-            //Arrange via setup
-
-            //Act
-            _Resettest.ResetBoard();
-
-            //Assert
-            Assert.IsNotNull(_Resettest.GetCoordinate(0, 0));
-
-            for (int y = 0; y < _Resettest.SizeY; y++)
-            {
-                for (int x = 0; x < _Resettest.SizeX; x++)
-                {
-                    Assert.IsInstanceOfType(_Resettest.GetCoordinate(x, y), typeof(PositionInfo));
-                }
-            }
-        }
-        [TestMethod]
-        public void ResetBoardShould
-    }
 }

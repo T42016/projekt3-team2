0e019e60493d21353fbc1469656e31a2fe28adfe
Kiwiov <hugo.hansen@lund.ntig.se>
2016-09-15 11:01:20 +0200

    Added tests for ClickCoordinate.

---
+++ MemoryTest/MemoryUnitTests/ClickCoordinate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using MemoryLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MemoryUnitTests
+{
+    [TestClass]
+    public class ClickCoordinate
+    {
+        [TestMethod]
+        public void ClickCoordinateShouldOpenCoordinate()
+        {
+            //Arange
+            MemoryGame game = new MemoryGame(5, 5);
+
+            //Act
+            game.ClickCoordinate(0, 0);
+
+            //Assert
+            Assert.AreEqual(true, game.GetCoordinate(0, 0).IsOpen);
+        }
+
+        [TestMethod]
+        public void ClickCoordinateShouldFindPair()
+        {
+            //Arange
+            MemoryGame game = new MemoryGame(1, 2);
+
+            //Act
+            game.ClickCoordinate(0, 0);
+            game.ClickCoordinate(0, 1);
+
+            //Assert
+            Assert.AreEqual(true, game.GetCoordinate(0, 0).IsFound);
+        }
+
+        [TestMethod]
+        public void ClickCoordinateShouldAddToDraws()
+        {
+            //Arange
+            MemoryGame game = new MemoryGame(5, 5);
+
+            //Act
+            game.ClickCoordinate(0, 0);
+            game.ClickCoordinate(0, 1);
+
+            //Assert
+            Assert.AreEqual(1, game.Draws);
+        }
+
+        [TestMethod]
+        public void ClickCoordinateShouldNotAddToDrawsYet()
+        {
+            //Arange
+            MemoryGame game = new MemoryGame(5, 5);
+
+            //Act
+            game.ClickCoordinate(0, 0);
+
+            //Assert
+            Assert.AreEqual(0, game.Draws);
+        }
+
+
+    }
+}

fd2c4d95436b83a481d7b169006c6e56d8146904
Kiwiov <hugo.hansen@lund.ntig.se>
2016-09-16 10:52:01 +0200

    added tests for win method.

---
+++ MemoryTest/MemoryUnitTests/Win.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MemoryLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MemoryUnitTests
+{
+    [TestClass]
+    public class Win
+    {
+        [TestMethod]
+        public void WinsWhenAllPairsAreDiscovered()
+        {
+            //Arrange
+            MemoryGame game = new MemoryGame(1,2);
+
+            //Act
+            game.ClickCoordinate(0,1);
+            game.ClickCoordinate(0,0);
+
+            //Assert
+            Assert.AreEqual(game.Points, game.Maxpoints);
+        }
+
+        [TestMethod]
+        public void MaxPointsIsSetCorrect()
+        {
+            //Arrange
+            MemoryGame game = new MemoryGame(3, 3);
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(game.Maxpoints, game.SizeX * game.SizeY / 2);
+        }
+    }
+}

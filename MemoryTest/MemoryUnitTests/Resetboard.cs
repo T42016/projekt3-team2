3cc8bad04775aed584962af4462cb8090dffa108
Marrezet <martin.dahren@lund.ntig.se>
2016-09-19 12:51:51 +0200

    ResetBoardShouldCheckIfValueHaveTwoZeros-sevens

--- MemoryTest/MemoryUnitTests/Resetboard.cs
+++ MemoryTest/MemoryUnitTests/Resetboard.cs
@@ -83,13 +83,164 @@ namespace MemoryUnitTests
             Assert.IsNotNull(_Resettest.GetCoordinate(0, 0));
         }
         [TestMethod]
-        public void ResetBoardShouldSetValueToPosition()
+        public void ResetBoardShouldCheckIfValueHaveTwoZeros()
         {
             //Arrange via setup
 
             //Act
+            _Resettest.ResetBoard();
+            int count = 0;
+            for (int y = 0; y < _Resettest.SizeY; y++)
+            {
+                for (int x = 0; x < _Resettest.SizeX; x++)
+                {
+                    if (_Resettest.GetCoordinate(x, y).Value == 0)
+                        count++;
+                }
+            }
 
             //Assert
+            Assert.AreEqual(2, count);
+        }
+        [TestMethod]
+        public void ResetBoardShouldCheckIfValueHaveTwoOnes()
+        {
+            //Arrange via setup
+
+            //Act
+            _Resettest.ResetBoard();
+            int count = 0;
+            for (int y = 0; y < _Resettest.SizeY; y++)
+            {
+                for (int x = 0; x < _Resettest.SizeX; x++)
+                {
+                    if (_Resettest.GetCoordinate(x, y).Value == 1)
+                        count++;
+                }
+            }
+
+            //Assert
+            Assert.AreEqual(2, count);
+        }
+        [TestMethod]
+        public void ResetBoardShouldCheckIfValueHaveTwoTwoes()
+        {
+            //Arrange via setup
+
+            //Act
+            _Resettest.ResetBoard();
+            int count = 0;
+            for (int y = 0; y < _Resettest.SizeY; y++)
+            {
+                for (int x = 0; x < _Resettest.SizeX; x++)
+                {
+                    if (_Resettest.GetCoordinate(x, y).Value == 2)
+                        count++;
+                }
+            }
+
+            //Assert
+            Assert.AreEqual(2, count);
+        }
+        [TestMethod]
+        public void ResetBoardShouldCheckIfValueHaveTwoThrees()
+        {
+            //Arrange via setup
+
+            //Act
+            _Resettest.ResetBoard();
+            int count = 0;
+            for (int y = 0; y < _Resettest.SizeY; y++)
+            {
+                for (int x = 0; x < _Resettest.SizeX; x++)
+                {
+                    if (_Resettest.GetCoordinate(x, y).Value == 3)
+                        count++;
+                }
+            }
+
+            //Assert
+            Assert.AreEqual(2, count);
+        }
+        [TestMethod]
+        public void ResetBoardShouldCheckIfValueHaveTwoFours()
+        {
+            //Arrange via setup
+
+            //Act
+            _Resettest.ResetBoard();
+            int count = 0;
+            for (int y = 0; y < _Resettest.SizeY; y++)
+            {
+                for (int x = 0; x < _Resettest.SizeX; x++)
+                {
+                    if (_Resettest.GetCoordinate(x, y).Value == 4)
+                        count++;
+                }
+            }
+
+            //Assert
+            Assert.AreEqual(2, count);
+        }
+        [TestMethod]
+        public void ResetBoardShouldCheckIfValueHaveTwoFives()
+        {
+            //Arrange via setup
+
+            //Act
+            _Resettest.ResetBoard();
+            int count = 0;
+            for (int y = 0; y < _Resettest.SizeY; y++)
+            {
+                for (int x = 0; x < _Resettest.SizeX; x++)
+                {
+                    if (_Resettest.GetCoordinate(x, y).Value == 5)
+                        count++;
+                }
+            }
+
+            //Assert
+            Assert.AreEqual(2, count);
+        }
+        [TestMethod]
+        public void ResetBoardShouldCheckIfValueHaveTwoSixes()
+        {
+            //Arrange via setup
+
+            //Act
+            _Resettest.ResetBoard();
+            int count = 0;
+            for (int y = 0; y < _Resettest.SizeY; y++)
+            {
+                for (int x = 0; x < _Resettest.SizeX; x++)
+                {
+                    if (_Resettest.GetCoordinate(x, y).Value == 6)
+                        count++;
+                }
+            }
+
+            //Assert
+            Assert.AreEqual(2, count);
+        }
+        [TestMethod]
+        public void ResetBoardShouldCheckIfValueHaveTwoSevens()
+        {
+            //Arrange via setup
+
+            //Act
+            _Resettest.ResetBoard();
+            int count = 0;
+            for (int y = 0; y < _Resettest.SizeY; y++)
+            {
+                for (int x = 0; x < _Resettest.SizeX; x++)
+                {
+                    if (_Resettest.GetCoordinate(x, y).Value == 7)
+                        count++;
+                }
+            }
+
+            //Assert
+            Assert.AreEqual(2, count);
         }
     }
 }
